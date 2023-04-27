5e0ffbef06b0f83e45d47322d5a66e2cec850200
Jeffri <csjeffri1994@gmail.com>
2023-04-27 18:28:34 +0200

    refactor(shared): Separate user menu in a component / add img

--- src/shared/components/Toolbar.vue
+++ src/shared/components/Toolbar.vue
@@ -13,23 +13,8 @@
           </ButtonApp>
           <ButtonApp v-if="employeeWorkStatus === 'online'" class="bg-red-400" @click="clockOut">Salir</ButtonApp>
           <span aria-hidden="true" class="block h-8 w-px rounded-full bg-gray-500"></span>
-          <button type="button" class="group flex shrink-0 items-center transition">
-            <img alt="Man"
-              src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-              class="h-10 w-10 rounded-full object-cover" />
-            <p class="ms-2 hidden text-left text-xs sm:block">
-              <strong class="block font-medium text-black">{{employeeFullName}}</strong>
-            </p>
-            <svg xmlns="http://www.w3.org/2000/svg"
-              class="ms-4 hidden h-5 w-5 text-gray-500 transition group-hover:text-gray-700 sm:block" viewBox="0 0 20 20"
-              fill="currentColor">
-              <path fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd" />
-            </svg>
-          </button>
+          <UserMenuApp :employee-full-name="employeeFullName" :work-status-employee="employeeWorkStatus" />
         </div>
-
       </div>
     </div>
   </div>
@@ -44,6 +29,7 @@
 import { computed, onMounted, ref, watch } from 'vue';
 import { useStore } from '../../store/index';
 import ButtonApp from './Button.vue';
+import UserMenuApp from './UserMenu.vue';
 import type { Coordinates, WorkEntriesPost } from '../../models/work-entries.model';
 import { workTimeHelper } from '../utils/helpers';
 const formatWorkTime = ref<string>("00:00:00");
@@ -100,6 +86,10 @@ const getClockInstance = (type: string) => {
         formatWorkTime.value = workTimeHelper(employeeWorkTime.value);
       }
       if (employeeWorkStatus.value === 'online' || employeeWorkStatus.value === 'pause') {
+        console.log(employeeWorkTime.value);
+        if(employeeWorkTime.value){
+          workTime.value = employeeWorkTime.value;
+        }
         counter.value = setInterval(() => {
           workTime.value += 1;
           formatWorkTime.value = workTimeHelper(workTime.value);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,37 @@
 <template>
-    <div class="text-black flex justify-center w-full h-full items-center p-4">
-        <h2>Home Component</h2>
+
+    <div class="flex w-full h-full justify-center items-center">
+        <a href="#" class="relative block overflow-hidden rounded-lg border border-gray-300 p-4 sm:p-6 lg:p-8">
+        <span class="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
+
+        <div class="sm:flex sm:justify-between sm:gap-4">
+            <div>
+                <h3 class="text-lg font-bold text-gray-900 sm:text-xl">
+                    Hola soy un Home component
+                </h3>
+
+                <p class="mt-1 text-xs font-medium text-gray-600">By Jeffri Villacreses</p>
+            </div>
+
+            <div class="hidden sm:block sm:shrink-0">
+                <img alt="Paul Clapton"
+                    src="../assets/img/imgjeff.png"
+                    class="h-16 w-16 rounded-lg object-cover shadow-sm" />
+            </div>
+        </div>
+
+        <div class="mt-4">
+            <p class="max-w-[40ch] text-sm text-gray-500">
+                Trabajo realizado con las siguiente herramientas y con el &#128153;:
+                Vue 3, Vue Router, TaildWindCSS, Typescript, Pinia. Coming soon....
+            </p>
+            
+
+                    
+        </div>
+    </a>
     </div>
+    
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
